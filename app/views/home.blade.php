91ef30e38f02ab873ebacf9ece8e0c5ecc1ed516
Angel Jimenez <ajimenez@digitalcaribe.com.co>
2014-04-16 23:21:33 -0500

    Agregado template de edit

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -5,14 +5,15 @@
     <title>Hands on Contacts</title>
     <style>
         table thead td { font-weight: bold; }
-        #addContact { margin-bottom:  2em; }
+        .module { margin-bottom:  2em; }
+
     </style>
 </head>
 <body>
 
 <h1>Contacts</h1>
 
-<form id="addContact">
+<form id="addContact" class="module">
     <div>
         <label for="first_name">First Name: </label>
         <input id="first_name" name="first_name" type="text"/>
@@ -35,22 +36,56 @@
     </div>
 </form>
 
-<table id="allContacts">
+<table id="allContacts" class="module">
     <thead>
         <tr>
             <td>First Name</td>
             <td>Last Name</td>
             <td>Email Address</td>
             <td>Description</td>
+            <td>Config</td>
         </tr>
     </thead>
 </table>
+
+<div id="editContact">
+
+</div>
 
 <script id="allContactsTemplate" type="text/x-handlebars-template">
     <td><%= first_name %></td>
     <td><%= last_name %></td>
     <td><%= email_address %></td>
     <td><%= description %></td>
+    <td><a href="#contacts/<%= id %>/edit" class="edit">Edit</a></td>
+    <td><a href="#contacts/<%= id %>" class="delete">Delete</a></td>
+</script>
+
+<script id="editContactTemplate" type="text/x-handlebars-template">
+    <h2>Edit Contact <%= first_name %> <%= last_name %> </h2>
+    <form id="editContact">
+        <div>
+            <label for="edit_first_name">First Name: </label>
+            <input id="edit_first_name" name="edit_first_name" type="text" value="<%= first_name %>"/>
+        </div>
+
+        <div>
+            <label for="edit_last_name">Last Name: </label>
+            <input id="edit_last_name" name="edit_last_name" type="text" value="<%= last_name %>" />
+        </div>
+        <div>
+            <label for="edit_email_address">Email Address:</label>
+            <input id="edit_email_address" name="edit_email_address" type="text" value="<%= email_address %>"/>
+        </div>
+        <div>
+            <label for="edit_description">Description:</label>
+            <textarea name="edit_description" id="edit_description" ><%= description %></textarea>
+        </div>
+        <div>
+            <input type="submit" value="Edit Contact"/>
+            <button type="button" class="cancel">Cancel</button>
+        </div>
+    </form>
 </script>
 
 <script src="js/jquery.js"></script>
